178e5620d7a45a5bb03ba71497d5e21db391a104
Matias Paez Molina <paezmolina@gmail.com>
2015-06-08 20:05:38 -0300

    Add directive to change src of image that may be missing and applied to product grid html.

--- public/modules/core/directives/core.client.directives.js
+++ public/modules/core/directives/core.client.directives.js
@@ -176,3 +176,17 @@ angular.module('core').directive('tab', function () {
         }
     }
 });
+
+angular.module('core').directive('errSrc', function() {
+    return {
+        link: function(scope, element, attrs) {
+            element.bind('error', function() {
+                if (attrs.src != attrs.errSrc) {
+                    attrs.$set('src', attrs.errSrc);
+                } else {
+                    attrs.$set('src','/design/images/noimage.jpg');
+                }
+            });
+        }
+    }
+});
