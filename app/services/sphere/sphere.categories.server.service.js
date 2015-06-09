bbe254129fc5e4c2083b74475c2add0de7490794
Sebastian Neira <sikian@gmail.com>
2015-06-09 18:10:50 +0200

    Category mapping on startup, breadcrumbs on product detail

--- app/services/sphere/sphere.categories.server.service.js
+++ app/services/sphere/sphere.categories.server.service.js
@@ -1,27 +1,64 @@
 'use strict';
 
-var SphereClient = require('../../clients/sphere.server.client.js'),
-  CommonService = require('./sphere.commons.server.service.js');
+var CommonService = require('./sphere.commons.server.service.js');
 
-var categories = {}
+var LANG = 'en';
 
-exports.getId = function(slug, callback){
-  if(categories.hasOwnProperty(slug)){
-    callback(null,categories[slug])
+var categories = []
+var catById = {}
+var catBySlug = {}
+
+exports.getId = function(slug){
+  if(catBySlug.hasOwnProperty(slug)){
+    return catBySlug[slug].id
   }else{
-    CommonService.getBySlug('categories', slug, function(err, result){
-      
-      if(err){
-        callback(err)
+    fetchCategories().then(function(){
+      if(catBySlug.hasOwnProperty(slug)){
+        return catBySlug[slug].id
       }else{
-        var id = result.results[0]['id']
-        if(id){
-          categories[slug] = result.results[0].id
-          callback(null, id)
-        }else{
-          callback(new Error("No id found for that slug"))
-        }
-      }        
+        return null
+      }
     })
   }
 }
+
+exports.getSlug = function(id){
+  if(catById.hasOwnProperty(id)){
+    return catById[id].slug[LANG]
+  }else{
+    fetchCategories().then(function(){
+      if(catById.hasOwnProperty(id)){
+        return catById[id].slug[LANG];
+      }else{
+        return null;
+      }
+    })
+  }
+}
+
+
+var fetchCategories = function(){
+  var p = new Promise(function(resolve, reject){
+    CommonService.all('categories', function(err, results){
+      if(err){
+        console.log(err)
+        reject(err)
+      }else{
+        categories = results;
+
+        for(var i = 0; i < categories.length; i++){
+          var cat = categories[i];
+          catById[cat.id] = cat;
+          catBySlug[cat.slug[LANG]] = cat;
+        }
+
+        resolve()
+      }
+    })
+  })
+
+  return p
+}
+
+// Run once on startup
+fetchCategories();
