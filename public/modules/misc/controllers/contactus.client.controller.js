593ffba115c2a60c351f6296e663bb599f1253a8
Sebastian Neira <sikian@gmail.com>
2015-06-26 12:15:06 +0200

    FOCALI-139 Make contactus controller for location and help

---
+++ public/modules/misc/controllers/contactus.client.controller.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('misc').controller('ContactusController', ['$scope', 'Authentication', '$http',
+    function ($scope, Authentication, $http) {
+        $scope.authentication = Authentication;
+
+        if($scope.authentication){
+            $scope.contactUs = {}
+            $scope.contactUs.email = $scope.authentication.user.email,
+                $scope.contactUs.name = $scope.authentication.user.name
+        }
+
+        $scope.contactUsSubmit = function(valid){
+            if(!valid){
+                $scope.contactError = "Please fill in all the required fields"
+            }
+
+            var post_body = {
+                name: $scope.contactUs.name,
+                email: $scope.contactUs.email,
+                message: $scope.contactUs.message
+            }
+
+            $http.post('/contactUs', post_body).success(function(result){
+                $scope.contactSuccess = $scope.contactError = null;
+
+                if(result.status == "sent"){
+                    $scope.contactSuccess = "Message sent successfully. We'll get back to you as soon as possible!"
+
+                    $scope.contactUs.message = ""
+                }else{
+                    $scope.contactError = "There was an error sending the email, please try again"
+                }
+            }).error(function(error){
+                $scope.contactError = "We seem to be having troubles on our end, please try again later"
+            })
+
+        }
+    }
+]);
--- public/modules/misc/controllers/locations.client.controller.js
+++ public/modules/misc/controllers/locations.client.controller.js
@@ -1,38 +1,8 @@
 'use strict';
 
-angular.module('misc').controller('LocationsController', ['$scope', 'Authentication', '$http',
-  function ($scope, Authentication, $http) {
+angular.module('misc').controller('LocationsController', ['$scope', 'Authentication',
+  function ($scope, Authentication) {
     $scope.authentication = Authentication;
 
-    if($scope.authentication){
-      $scope.contactUs = {}
-      $scope.contactUs.email = $scope.authentication.user.email,
-      $scope.contactUs.name = $scope.authentication.user.name
-    }
-
-    $scope.contactUsSubmit = function(valid){
-      if(!valid){
-        $scope.contactError = "Please fill in all the required fields"
-      }
-
-      var post_body = {
-        name: $scope.contactUs.name,
-        email: $scope.contactUs.email,
-        message: $scope.contactUs.message
-      }
-
-      $http.post('/contactUs', post_body).success(function(result){
-        $scope.contactSuccess = $scope.contactError = null;
-
-        if(result.status == "sent"){
-          $scope.contactSuccess = "Message sent successfully. We'll get back to you as soon as possible!"
-        }else{
-          $scope.contactError = "There was an error sending the email, please try again"
-        }
-      }).error(function(error){
-        $scope.contactError = "We seem to be having troubles on our end, please try again later"
-      })
-
-    }
   }
 ]);
