eaab6ffcebfc8ca16c441356b6445bc9c2aee1d4
Emanuel Perez <emaperez@gmail.com>
2016-07-27 10:39:21 -0300

    Process html content from contentful on all pages

--- public/modules/misc/controllers/privacy.client.controller.js
+++ public/modules/misc/controllers/privacy.client.controller.js
@@ -1,8 +1,12 @@
 'use strict';
 
-angular.module('misc').controller('PrivacyController', ['$scope', 'Authentication','ContentfulService',
-    function ($scope, Authentication,ContentfulService) {
+angular.module('misc').controller('PrivacyController', ['$scope', 'Authentication','ContentfulService','$sce',
+    function ($scope, Authentication,ContentfulService,$sce) {
         $scope.authentication = Authentication;
+
+        $scope.trustAsHtml = function (string) {
+            return $sce.trustAsHtml(string);
+        };
 
         $scope.loadContent = function(){
             ContentfulService['byTypeAndName']('miscPage','Privacy Page').then(function(result) {
--- public/modules/misc/controllers/terms-of-use.client.controller.js
+++ public/modules/misc/controllers/terms-of-use.client.controller.js
@@ -1,8 +1,12 @@
 'use strict';
 
-angular.module('misc').controller('TermsOfUseController', ['$scope', 'Authentication','ContentfulService',
-    function ($scope, Authentication,ContentfulService) {
+angular.module('misc').controller('TermsOfUseController', ['$scope', 'Authentication','ContentfulService','$sce',
+    function ($scope, Authentication,ContentfulService,$sce) {
         $scope.authentication = Authentication;
+
+        $scope.trustAsHtml = function (string) {
+            return $sce.trustAsHtml(string);
+        };
 
         $scope.loadContent = function(){
             ContentfulService['byTypeAndName']('miscPage','Terms of Use').then(function(result) {
