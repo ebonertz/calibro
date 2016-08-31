bcc009c37d8a68d906d9a2e0c99685601c4d0bdd
Emanuel Perez <emaperez@gmail.com>
2016-08-31 13:49:35 -0300

    Change production server path

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/focali-mean',
-	serverPath: 'http://focali-stage.ct-app.com/',
+	serverPath: 'https://focalioptics.com/',
 	assets: {
 		lib: {
 			css: 'public/dist/vendor.min.css',
