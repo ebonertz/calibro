693ed774ac77c5f85e6521471a8b6fb17b990de2
Emanuel Perez <ema@devgurus.io>
2016-09-08 10:43:46 -0300

    Modify how files are minifies and uglified

--- gruntfile.js
+++ gruntfile.js
@@ -2,8 +2,10 @@
 
 var init = require('./config/init')();
 var config = require('./config/config');
+var defaultAssets = require ('./config/env/all');
 
-module.exports = function(grunt) {
+
+	module.exports = function(grunt) {
 	// Unified Watch Object
 	var watchFiles = {
 		serverViews: ['app/views/**/*.*'],
@@ -82,7 +84,7 @@ module.exports = function(grunt) {
 			combine: {
 				files: {
 					'public/dist/application.min.css': '<%= applicationCSSFiles %>',
-					'public/dist/vendor.min.css': config.assets.lib.css
+					'public/dist/vendor.min.css': defaultAssets.assets.lib.css
 				}
 			}
 		},
@@ -92,7 +94,7 @@ module.exports = function(grunt) {
 					stripBanners: true
 				},
 				files: {
-					'public/dist/vendor.min.js': config.assets.lib.js
+					'public/dist/vendor.min.js': defaultAssets.assets.lib.js
 				}
 			}
 		},
@@ -169,8 +171,8 @@ module.exports = function(grunt) {
 	// A Task for loading the configuration object
 	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
 
-		grunt.config.set('applicationJavaScriptFiles', config.assets.js);
-		grunt.config.set('applicationCSSFiles', config.assets.css);
+		grunt.config.set('applicationJavaScriptFiles', defaultAssets.assets.js);
+		grunt.config.set('applicationCSSFiles', defaultAssets.assets.css);
 	});
 
 	// Default task(s).
